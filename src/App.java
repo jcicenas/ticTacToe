e88b6c59cf1f1e7fde17adfb8a1fc9e3069fb6b3
John Cicenas <jcicenas2@gmail.com>
2024-01-22 09:41:20 -0500

    asda

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.*;
 import javax.sound.sampled.SourceDataLine;
 public class App {
@@ -6,6 +5,7 @@ public class App {
     Scanner scan = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         displayMethod();
+        App m1 = new App();
     }
 
     public static void displayMethod(){
@@ -19,5 +19,24 @@ public class App {
         App m1 = new App();
         int row = InputHelper.getRangedInt(m1.scan, "Enter move row [1 - 3]", 1, 3);
         int column = InputHelper.getRangedInt(m1.scan, "Enter move column [1 - 3]", 1, 3);
+    }
+
+
+    public static int getRandomIntNotInMap(HashMap<Integer, Character> map){
+        Boolean s = true;
+        do{
+            int randomNum = 1 + (int)(Math.random() * ((9 - 1) + 1));
+            if(map.get(randomNum) == 'X' || map.get(randomNum) == 'O'){
+                s = false;
+            }else{
+                return randomNum;
+            }
+        }while(s);
+        return -1;
+
+    }
+    public static void rbtFlow(int randomNum, HashMap<Integer, Character> map){
+        ArrayList<Integer> testList = new ArrayList<>();
+        testList.add(ccounter1);
     }
 }
